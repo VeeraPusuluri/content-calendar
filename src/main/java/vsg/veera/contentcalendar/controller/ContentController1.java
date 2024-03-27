3b0c50a020383205b5a9e7245fb80ff6aae24fc4
Veera <veera@Veeras-MacBook-Pro.local>
2024-03-27 16:29:06 +0530

    commiting changes

--- src/main/java/vsg/veera/contentcalendar/controller/ContentController1.java
+++ src/main/java/vsg/veera/contentcalendar/controller/ContentController1.java
@@ -2,11 +2,13 @@ package vsg.veera.contentcalendar.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-@RestController
+@RestController()
+@RequestMapping("/configTest")
 public class ContentController1 {
 
     @Value("${cc.Message}")
